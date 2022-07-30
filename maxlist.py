17f6b78f66a98870d26e8cf64c716ddf5a4ca264
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-07-30 07:44:02 +0530

    adding new python files

---
+++ maxlist.py
@@ -0,0 +1,15 @@
+lst = [10,203,2,30,203]
+
+print(lst)
+
+print("MAX of the list")
+
+print(max(list))
+
+# method2
+
+lst.sort()
+
+print(lst[-1])
+
+
---
+++ minlist.py
@@ -0,0 +1,15 @@
+lst = [10,203,2,30,203]
+
+print(lst)
+
+print("MINIMUM of the LIST")
+
+print(min(lst))
+
+print("Sum of the LIST")
+print(sum(lst))
+
+for data in lst:
+    print(data , end = " ")
+
+
